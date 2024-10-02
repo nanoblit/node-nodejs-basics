e5d355a368a22fad8d5a404e5786157832f81df6
nanoblit <jakub.k.maleta@gmail.com>
2024-10-02 20:25:15 +0200

    feat: finish copy.js subtask

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
 const copy = async () => {
-    // Write your code here 
+  const originalFolderPath = path.join(import.meta.dirname, "files");
+  const newFolderPath = path.join(import.meta.dirname, "files_copy");
+
+  const newFolderExists = await fs
+    .stat(newFolderPath)
+    .then(() => true)
+    .catch((e) => false);
+
+  if (newFolderExists) {
+    throw createfileSystemError();
+  }
+
+  try {
+    await fs.access(originalFolderPath, fs.constants.R_OK);
+    await fs.cp(originalFolderPath, newFolderPath, { recursive: true });
+  } catch (e) {
+    throw createfileSystemError();
+  }
 };
+
+function createfileSystemError() {
+  return new Error("FS operation failed");
+}
 
 await copy();
