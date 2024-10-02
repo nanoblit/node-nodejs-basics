be6a1822a05fd06e68aedd2fab2085f15376cf2c
nanoblit <jakub.k.maleta@gmail.com>
2024-10-02 20:59:44 +0200

    feat: finish read.js subtask

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join(import.meta.dirname, "files", "fileToRead.txt");
+
+  try {
+    const file = await fs.open(filePath, "r");
+    const contents = await file.readFile({ encoding: "utf-8" });
+    console.log(contents);
+  } catch (e) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();
