c1e55847983fc4241d4835dd0258fd5689cda7b2
nanoblit <jakub.k.maleta@gmail.com>
2024-10-05 16:34:12 +0200

    feat: finish hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import fs from "node:fs/promises";
+import { createHash } from "node:crypto";
+import path from "node:path";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = path.join(import.meta.dirname, "files", "fileToCalculateHashFor.txt");
+  let contents;
+
+  try {
+    const file = await fs.open(filePath, "r");
+    contents = await file.readFile({ encoding: "utf-8" });
+  } catch (e) {
+    throw new Error("FS operation failed");
+  }
+
+  const hashedContents = createHash("sha256").update(contents).digest("hex");
+  process.stdout.write(hashedContents);
 };
 
 await calculateHash();
