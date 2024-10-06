ee72e0c4281a56722a6563b9c8844e6a5d82c3e0
nanoblit <jakub.k.maleta@gmail.com>
2024-10-06 12:39:27 +0200

    feat: finish subtask write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { pipeline } from "node:stream/promises";
+import { createWriteStream } from "node:fs";
+import path from "node:path";
+
 const write = async () => {
-    // Write your code here 
+  const filePath = path.join(import.meta.dirname, "files", "fileToWrite.txt");
+
+  try {
+    await pipeline(process.stdin, createWriteStream(filePath));
+  } catch (e) {
+    throw new Error("Pipeline has failed");
+  }
 };
 
 await write();
