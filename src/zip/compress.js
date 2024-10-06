9af0f4848d182893214b47321731449b841c4bed
nanoblit <jakub.k.maleta@gmail.com>
2024-10-06 13:20:13 +0200

    feat: finish compress.js subtask

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,14 @@
+import { createGzip } from "node:zlib";
+import { pipeline } from "node:stream/promises";
+import { createReadStream, createWriteStream } from "node:fs";
+import path from "node:path";
+
 const compress = async () => {
-    // Write your code here 
+  const sourcePath = path.join(import.meta.dirname, "files", "fileToCompress.txt");
+  const destinationPath = path.join(import.meta.dirname, "files", "archive.gz");
+  const gzip = createGzip();
+
+  pipeline(createReadStream(sourcePath), gzip, createWriteStream(destinationPath));
 };
 
 await compress();
